1e5c6cedb2c41300b3b38355377bddc13ca6d688
n-fukidome <n-fukidome@nekojarashi.com>
2014-04-16 13:01:20 +0900

    create country-model
    scaffoldでモデル作成
    
    Signed-off-by: n-fukidome <n-fukidome@nekojarashi.com>

---
+++ app/views/countries/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @country, :id, :name, :market_scale, :product_stock, :market_segment, :buying_prise, :max_selling_price, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Nmg::Application.routes.draw do
+  resources :countries
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
