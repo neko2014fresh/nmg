d1926d5e9910a150472c24abea969c3dd329b7ac
Takuya-Kaneko <t-kaneko@nekojarashi.com>
2014-04-28 14:12:45 +0900

    show mypage_data

--- app/controllers/mypage_controller.rb
+++ app/controllers/mypage_controller.rb
@@ -1,11 +1,10 @@
 class MypageController < ApplicationController
   
   def show_data
-    # データがひとつの場合を想定 
-    user_data = User.first
-    @email = user_data.email
-    @net_profit = user_data.net_profit
-    @cashe = user_data.cashe
+    @user_data = User.all
+    # @email = user_data.email
+    # @net_profit = user_data.net_profit
+    # @cashe = user_data.cashe
   end
 
 end
